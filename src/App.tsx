4dd611e0592db223edc6345d84cfa70212f2501e
Svitwave <svitlanamaz@gmail.com>
2025-11-01 15:11:35 +0200

    done homework

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import CafeInfo from "./components/CafeInfo";
+import styles from "./App.module.css";
+import type { Votes, VoteType } from "./types/votes";
+import { useState } from "react";
+import VoteOptions from "./VoteOptions/VoteOptions";
+import VoteStats from "./VoteOptions/VoteStats";
+import Notification from "./components/Notification";
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const canShowReset = votes.good > 0 || votes.neutral > 0 || votes.bad > 0;
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+  return (
+    <>
+      <div className={styles.app}>
+        <CafeInfo />
+        <VoteOptions
+          onVote={handleVote}
+          onReset={resetVotes}
+          canReset={canShowReset}
+        />
+        {totalVotes > 0 ? (
+          <VoteStats
+            votes={votes}
+            totalVotes={totalVotes}
+            positiveRate={positiveRate}
+          />
+        ) : (
+          <Notification />
+        )}
+      </div>
+    </>
+  );
+}
+export default App;
---
+++ src/VoteOptions/VoteStats.tsx
@@ -0,0 +1,34 @@
+import styles from "./VoteStats.module.css";
+import type { Votes } from "../types/votes";
+
+interface VoteStatsProps {
+  votes: Votes;
+  totalVotes: number;
+  positiveRate: number; // in percent (e.g. 50 for 50%)
+}
+
+function VoteStats({ votes, totalVotes, positiveRate }: VoteStatsProps) {
+  const positiveLabel = `${Math.round(positiveRate)}%`;
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.stat}>
+        Good: <strong>{votes.good}</strong>
+      </p>
+      <p className={styles.stat}>
+        Neutral: <strong>{votes.neutral}</strong>
+      </p>
+      <p className={styles.stat}>
+        Bad: <strong>{votes.bad}</strong>
+      </p>
+      <p className={styles.stat}>
+        Total: <strong>{totalVotes}</strong>
+      </p>
+      <p className={styles.stat}>
+        Positive: <strong>{positiveLabel}</strong>
+      </p>
+    </div>
+  );
+}
+
+export default VoteStats;
